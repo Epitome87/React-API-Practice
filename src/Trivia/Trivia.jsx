dd840ffb4592e55c5dbd0a50668e7239ec49afdd
Matthew McGrath <rubywep@hotmail.com>
2021-12-09 14:32:32 -0800

    Add some minor background, font, and layout styling to the Trivia API app. Fixed issue where the retrieved Trivia data was in an unpleasant to read format. Add ability to score upon correct answers -- though there is no feedback on which questions were answered right or wrong.

--- src/Trivia/Trivia.jsx
+++ src/Trivia/Trivia.jsx
@@ -1,34 +1,46 @@
-import React, { useEffect, useState } from "react";
-import TriviaItem from "./TriviaItem";
-import axios from "axios";
-import classes from "./Trivia.module.css";
+import React, { useEffect, useState } from 'react';
+import TriviaItem from './TriviaItem';
+import axios from 'axios';
+import classes from './Trivia.module.css';
 
 // Represents a Trivia game. Keeps track of a list of Trivia questions
 function Trivia() {
+  const NUM_QUESTIONS = 10;
   const [isLoading, setIsLoading] = useState(true);
   const [triviaQuestions, setTriviaQuestions] = useState([]);
   const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
   const [score, setScore] = useState(0);
 
   useEffect(() => {
-    fetchTrivia(10);
+    fetchTrivia(NUM_QUESTIONS);
   }, []);
 
   const fetchTrivia = async (numTrivia) => {
     const data = await axios.get(
-      `https://opentdb.com/api.php?amount=${numTrivia}`
+      `https://opentdb.com/api.php?amount=${numTrivia}&encode=base64`
     );
 
-    setTriviaQuestions(data.data.results);
+    // Transform API's returned data to better suit our use case
+    const transformedData = data.data.results.map((result) => {
+      return {
+        incorrectAnswers: result['incorrect_answers'].map((wrongAnswer) =>
+          atob(wrongAnswer)
+        ),
+        correctAnswer: atob(result['correct_answer']),
+        question: atob(result.question),
+      };
+    });
+
+    setTriviaQuestions(transformedData);
     setIsLoading(false);
   };
 
   const handleSubmitAnswer = (guess) => {
-    if (guess === triviaQuestions[currentQuestionNumber]["correct_answer"]) {
+    if (guess === triviaQuestions[currentQuestionNumber].correctAnswer) {
       setScore((score) => score + 1);
       setCurrentQuestionNumber((num) => num + 1);
     } else {
-      console.log("WRONG");
+      console.log('WRONG');
     }
   };
 
@@ -36,15 +48,19 @@ function Trivia() {
 
   return (
     <section className={classes.Trivia}>
+      <p className={classes.questionNumber}>
+        Question #{currentQuestionNumber + 1} of {NUM_QUESTIONS}
+      </p>
       <TriviaItem
         questionNumber={currentQuestionNumber + 1}
         question={triviaQuestions[currentQuestionNumber].question}
-        correctAnswer={triviaQuestions[currentQuestionNumber]["correct_answer"]}
+        correctAnswer={triviaQuestions[currentQuestionNumber].correctAnswer}
         incorrectAnswers={
-          triviaQuestions[currentQuestionNumber]["incorrect_answers"]
+          triviaQuestions[currentQuestionNumber].incorrectAnswers
         }
         handleClickAnswer={handleSubmitAnswer}
       />
+      <p className={classes.score}>Score: {score}</p>
       <button onClick={handleSubmitAnswer}>Skip Question</button>
     </section>
   );
